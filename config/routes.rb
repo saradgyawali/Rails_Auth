02618f30d70bbc8260868076e562bca4dd118488
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-22 09:36:28 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+ root to: "welcome#index"
+
+  get "/login", to: "sessions#new"
+
+  post "/sessions", to: "sessions#create"
+
+  get "/sign_up", to: "users#new", as: "sign_up"
+
+  resources :users
+
+end
